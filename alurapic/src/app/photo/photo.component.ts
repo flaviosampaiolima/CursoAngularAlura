6b20ed001df3c7ad5daa4a4a909fada71a47b9ca
Flavio Sampaio Reis de Lima <flaviosampaioreisdelima@MacBook-Air-de-Flavio.local>
2018-09-23 17:25:11 -0300

    Criado o componente Photo

---
+++ alurapic/src/app/photo/photo.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent {
+
+    @Input() url = '';
+
+    @Input() descricao = '';
+
+}
